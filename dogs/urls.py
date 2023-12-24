f00d9dcd84d434102b7bcc9ee33162c431e87917
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-24 20:47:31 +0300

    commit

--- dogs/urls.py
+++ dogs/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('dogs/<int:pk>/', DogListView.as_view(), name='category_dogs'),
-    path('dogs/create/', DogCreateView.as_view(), name='dog_create'),
+    path('<int:pk>/dogs/create/', DogCreateView.as_view(), name='dog_create'),
     path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='dog_update'),
     path('dogs/delete/<int:pk>/', DogDeleteView.as_view(), name='dog_delete')
 ]
